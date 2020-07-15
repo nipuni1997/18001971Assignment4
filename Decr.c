65237c01464cbe8995edb7ffbe64020236957964
nipuni1997 <54950127+nipuni1997@users.noreply.github.com>
2020-07-16 00:07:30 +0530

    Add files via upload

---
+++ Decr.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+	char array[50];
+	char ch;
+	int i=0 ,key;
+	printf("Enter a Encrypted Code:");
+	scanf("%s",&array[i]);
+	printf("Enter key: ");
+	scanf("%d",&key);
+	while(array[i]!= '\0'){
+		ch=array[i];
+		if(ch >='a' && ch <= 'z'){
+			ch=ch-key;
+			array[i]= ch;
+		}else if(ch >='A' && ch <= 'Z'){
+			ch=ch-key;
+			array[i]=ch;
+		}
+		i++;
+	} 
+	printf("Decrypted Code: %s", array);
+	return 0;
+}
